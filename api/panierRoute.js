e3f1210797999a3e3512e9c953c3db2d14686a2d
= <=>
2023-03-02 11:44:48 +0100

    use mysql db + add the checkout and discount logic

--- api/panierRoute.js
+++ api/panierRoute.js
@@ -1,28 +1,47 @@
 import PanierService from "../services/panierService.js";
+import ProduitService from "../services/produitService.js";
 
 
 export default app => {
     const panierService = new PanierService();
+    const productService = new ProduitService();
 
-    app.get("/panier/:id", (req, res, next) => {
 
-    })
-
-    app.post("/panier", (req, res, next) => {
+    app.post('/api/:idPanier/add', (req, res) => {
+        const { idPanier } = req.params;
         
-    })
+        //find the cart
+        const panier = panierService.findCart({ idCart: idPanier });
+
+        const { productCode, quantity } = req.body;
+        // TODO: Validate productCode and quantity
+      
+        // Get product price from database
+        const product = productService.getProductByCode(productCode);
+        const price = product.price;
+      
+        // Add product to cart object
+        let prod =  panierService.findProductInCart(productCode);
+        
+        if (prod) 
+            panierService.updateProductQuantity({idCart: idPanier , idProduct: productCode });
+        else 
+            panierService.addProductToPanier({ produitId: productCode })
+        
+      
+        // Return updated cart object
+        res.status(200).send({ message: "product added to cart" });
+      });
+      
 
 
-    // add product to a cart
-    app.post("/panier/add-to-cart/:produitId", (req, res, next) => {
-        const { produitId } = req.params;
-        panierService.addProductToPanier({ produitId });
-    })
-
-    // check out
-    app.post("/panier/:panierId/check-out", (req, res, next) => {
-        const { panierId } = req.params;
-        panierService.checkOut({ panierId });
-    })
-
+      // checkout
+      app.post('/api/:idPanier/checkout', async (req, res) => {
+        // Calculate total and discounts
+        const { total, discountAmount } =  panierService.calculateTotal();
+      
+        // Return total and discounts applied
+        res.json({ total, discountAmount });
+      });
+      
 }
